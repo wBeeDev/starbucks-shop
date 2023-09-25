0e45df4979c3c3cd972cb1f45f50e521a573b6b1
wBeeDev <hiryuji@kakao.com>
2023-09-25 18:03:24 +0900

    Font 수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,8 +8,12 @@ export default function Banner() {
         <h2 className="pb-4 text-6xl font-semibold text-black drop-shadow-2xl">
           Joyful Memories Together
         </h2>
-        <p className="text-2xl font-semibold text-black drop-shadow-2xl">
-          디즈니 콜라보레이션 상품을 소개합니다.
+
+        <p className="text-2xl font-semibold text-black drop-shadow-xl">
+          <span className="mr-2 text-2xl font-black text-brand drop-shadow-xl">
+            디즈니 콜라보레이션
+          </span>
+          상품을 소개합니다.
         </p>
       </div>
     </section>
